a93fb00748854db9c4ed146451a88fa7a6da8128
Chip Nielsen <chip.nielsen@netguru.dev>
2021-05-24 21:59:27 -0700

    Implement Vault client with approle auth

--- nautobot_plugin_nornir/plugins/credentials/hashicorp_vault.py
+++ nautobot_plugin_nornir/plugins/credentials/hashicorp_vault.py
@@ -1,5 +1,9 @@
 """Credentials class for HashiCorp Vault passwords."""
+import os
+import hvac
+
 from .nautobot_orm import MixinNautobotORMCredentials
+
 
 class HashiCorpVaultVars(MixinNautobotORMCredentials):
     """Credentials Class designed to work with Nautobot ORM.
@@ -20,8 +24,17 @@ class HashiCorpVaultVars(MixinNautobotORMCredentials):
             params = {}
 
         if not isinstance(params, dict):
-            raise TypeError("params must be a dictionnary")
+            raise TypeError("params must be a dictionary")
 
         self.username = ""
         self.password = ""
         self.secret = ""
+
+        self.url = params.get("vault_url", os.environ["VAULT_ADDR"])
+        # Namespace is optional
+        self.namespace = params.get("vault_namespace", os.getenv("VAULT_NAMESPACE"))
+        self.role_id = params.get("vault_role_id", os.environ["VAULT_ROLE_ID"])
+        self.secret_id = params.get("vault_secret_id", os.environ["VAULT_SECRET_ID"])
+        self.client = hvac.Client(
+            url=self.url, role_id=self.role_id, secret_id=self.secret_id, namespace=self.namespace
+        )
