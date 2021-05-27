0c0d3bf1bcaa6e4c7dc16f2333ea3759674145c7
Chip Nielsen <chip.nielsen@netguru.dev>
2021-05-26 17:48:18 -0700

    Update to use PLUGIN_SETTINGS

--- nautobot_plugin_nornir/plugins/credentials/hashicorp_vault.py
+++ nautobot_plugin_nornir/plugins/credentials/hashicorp_vault.py
@@ -2,7 +2,10 @@
 import os
 import hvac
 
+from django.conf import settings
 from .nautobot_orm import MixinNautobotORMCredentials
+
+PLUGIN_SETTINGS = settings.PLUGINS_CONFIG["nautobot_plugin_nornir"]
 
 
 class HashiCorpVaultVars(MixinNautobotORMCredentials):
@@ -26,15 +29,19 @@ class HashiCorpVaultVars(MixinNautobotORMCredentials):
         if not isinstance(params, dict):
             raise TypeError("params must be a dictionary")
 
-        self.username = ""
-        self.password = ""
-        self.secret = ""
-
-        self.url = params.get("vault_url", os.environ["VAULT_ADDR"])
+        self.url = PLUGIN_SETTINGS.get("vault_url", os.environ["VAULT_ADDR"])
         # Namespace is optional
-        self.namespace = params.get("vault_namespace", os.getenv("VAULT_NAMESPACE"))
-        self.role_id = params.get("vault_role_id", os.environ["VAULT_ROLE_ID"])
-        self.secret_id = params.get("vault_secret_id", os.environ["VAULT_SECRET_ID"])
+        self.namespace = PLUGIN_SETTINGS.get("vault_namespace", os.getenv("VAULT_NAMESPACE"))
+        self.role_id = PLUGIN_SETTINGS.get("vault_role_id", os.environ["VAULT_ROLE_ID"])
+        self.secret_id = PLUGIN_SETTINGS.get("vault_secret_id", os.environ["VAULT_SECRET_ID"])
+        self.nautobot_secret_path = PLUGIN_SETTINGS.get("nautobot_secret_path", os.environ["NAUTOBOT_SECRET_PATH"])
         self.client = hvac.Client(
             url=self.url, role_id=self.role_id, secret_id=self.secret_id, namespace=self.namespace
         )
+
+        self.secrets = self.client.secrets.kv.read_secret_version(path=self.nautobot_secret_path)
+        self.creds = self.secrets["data"]["data"]
+
+        self.username = self.creds["username"]
+        self.password = self.creds["password"]
+        self.secret = self.creds.get("secret", self.password)
